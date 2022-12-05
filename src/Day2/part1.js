c419e70d03feaed8ba6f85149ba81cd2e73c488c
Medy <krouk.medy@gmail.com>
2022-12-05 12:42:58 +0100

    update and finish of part1 - Day2.

--- src/Day2/part1.js
+++ src/Day2/part1.js
@@ -29,20 +29,15 @@ C > Y = 0
 C = Z = 3
 
 
-if (x){ScoreShape = 1 } and if (A < Y || B < Z || C < X ) {ScoreRound = 6} elseif (A = X  || B = Y || C = Z) {ScoreRound = 6} else {scoreRound = 0}
-if (y){ScoreShape = 2 } and if
-if (z) {ScoreShape = 3 } and if
-
-
-
-
-
+if (x){scoreShape = 1 } and if (A < Y || B < Z || C < X ) {ScoreRound = 6} elseif (A = X  || B = Y || C = Z) {ScoreRound = 6} else {scoreRound = 0}
+if (y){scoreShape = 2 } and if
+if (z) {scoreShape = 3 } and if
 
 const lost = 0;
 const draw = 3;
 const win = 6;
 
-resultRound = pointShape + pointRound
+resultRound = scoreShape + scoreRound
 resultTotal = sum(resultRound)
 */
 
@@ -61,12 +56,120 @@ const fileContent = await fs.readFile('puzzle_file', 'utf8');
 
 
 const PACKAGE = '\n';
+const SHAPES = ' '
 
 const gameScript = splitStr(fileContent, PACKAGE);
 
-console.log(gameScript);
+/* console.log(gameScript); */
+
+var scoreShape = 0
+var scoreRound = 0
+
 
 gameScript.forEach((round) => {
-    /*  */
+
+    const shape = round.split(' ');
+
+    if (shape[1] === 'X') {
+        scoreShape += 1;
+        console.log('rock');
+    }
+    if (shape[1] === 'Y') {
+        scoreShape += 2;
+        console.log('paper');
+    }
+    if (shape[1] === 'Z') {
+        scoreShape += 3
+        console.log('scissors');
+    }
+
+
+    /* switch (shape[1]) {
+        case 'X':
+            scoreShape += 1;
+            console.log('rock');
+        case 'Y':
+            scoreShape += 2;
+            console.log('paper');
+        case 'Z':
+            scoreShape += 3
+            console.log('scissors');
+            break;
+    } */
+
+    if (shape[0] === 'A' && shape[1] === 'X') {
+        scoreRound += 3;
+        console.log("Nul");
+    }
+    if (shape[0] === 'B' && shape[1] === 'Y') {
+        scoreRound += 3;
+        console.log("Nul");
+    }
+    if (shape[0] === 'C' && shape[1] === 'Z') {
+        scoreRound += 3;
+        console.log("Nul");
+    }
+    if (shape[0] === 'A' && shape[1] === 'Y') {
+        scoreRound += 6;
+        console.log("Gagné");
+    }
+    if (shape[0] === 'B' && shape[1] === 'Z') {
+        scoreRound += 6;
+        console.log("Gagné");
+    }
+    if (shape[0] === 'C' && shape[1] === 'X') {
+        scoreRound += 6;
+        console.log("Gagné");
+    }
+    if (shape[0] === 'A' && shape[1] === 'Z') {
+        scoreRound += 0;
+        console.log("Perdu");
+    }
+    if (shape[0] === 'B' && shape[1] === 'X') {
+        scoreRound += 0;
+        console.log("Perdu");
+    }
+    if (shape[0] === 'B' && shape[1] === 'Y') {
+        scoreRound += 0;
+        console.log("Perdu");
+    }
+
+    /*  switch (shape[0], shape[1]) {
+         case 'A' && 'X':
+             scoreRound += 3;
+             console.log("Nul");
+         case 'A' && 'Y':
+             scoreRound += 6;
+             console.log("Gagné");
+         case 'A' && 'Z':
+             scoreRound += 0;
+             console.log("Perdu");
+         case 'B' && 'Y':
+             scoreRound += 3;
+             console.log("Nul");
+         case 'B' && 'Z':
+             scoreRound += 6;
+             console.log("Gagné");
+         case 'B' && 'X':
+             scoreRound += 0;
+             console.log("Perdu");
+         case 'C' && 'Z':
+             scoreRound += 3;
+             console.log("Nul");
+         case 'C' && 'X':
+             scoreRound += 6;
+             console.log("Gagné");
+         case 'C' && 'Y':
+             scoreRound += 0;
+             console.log("Perdu");
+             break;
+     } */
+
+
 
 });
+
+console.log(scoreShape);
+console.log(scoreRound);
+var resultTotal = scoreShape + scoreRound;
+console.log(resultTotal);
